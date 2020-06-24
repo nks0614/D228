349f21911fae2bb186500ce3842daecf2d8a8be8
sh030128 <junghunsung01@gmail.com>
2020-06-24 19:31:35 +0900

    [ADD] login layout
    로그인 html 코드 추가

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: '228 기념사업회' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "228 기념사업회" });
 });
 
-router.get('/member/list', function(req, res, next) {
-  res.render('member/list');
+router.get("/member/list", function (req, res, next) {
+  res.render("member/list");
+});
+
+router.get("/login", function (req, res, next) {
+  res.render("login/index");
 });
 
 module.exports = router;
